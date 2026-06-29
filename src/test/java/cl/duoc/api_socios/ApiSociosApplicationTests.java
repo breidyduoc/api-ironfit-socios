2c8f2b6fb5688c8e7a769744ee5d5362a478b0ca
breidyduoc <br.pimienta@duocuc.cl>
2026-06-28 23:20:56 -0400

    ajuste final de testeo y application.yaml para entrega

--- src/test/java/cl/duoc/api_socios/ApiSociosApplicationTests.java
+++ src/test/java/cl/duoc/api_socios/ApiSociosApplicationTests.java
@@ -1,9 +1,10 @@
 package cl.duoc.api_socios;
 
+import cl.duoc.api_ironfit_socios.ApiSociosApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = ApiSociosApplication.class)
 class ApiSociosApplicationTests {
 
 	@Test
